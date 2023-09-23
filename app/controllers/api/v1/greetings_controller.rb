f5632538025f941c9306ba10f625192788ade485
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-09-23 22:53:17 +0100

    Update controllers and routes

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::GreetingsController < ApplicationController
-  def random
+  def index
     random_greeting = Message.order('RANDOM()').first
     render json: { message: random_greeting.greeting }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      resources :greetings do
-        collection do
-          get 'random'
-        end
-      end
+      resources :greetings 
     end
   end
 end
