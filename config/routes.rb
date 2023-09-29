e08f9758b49032e4c1667849a6b7db7a97d6cbdc
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-09-29 17:16:04 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      resources :greetings 
+       get '/greetings/random', to: 'greetings#random'
     end
   end
 end
